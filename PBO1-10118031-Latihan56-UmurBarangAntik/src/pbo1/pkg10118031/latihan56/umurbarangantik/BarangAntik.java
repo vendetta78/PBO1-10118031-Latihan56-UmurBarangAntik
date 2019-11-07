348db8d7a5a9a18efda22951a1246c0d2cc187c5
Rizki Aria Putra <55823091+vendetta78@users.noreply.github.com>
2019-11-07 22:17:15 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan56-UmurBarangAntik/src/pbo1/pkg10118031/latihan56/umurbarangantik/BarangAntik.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118031.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author Vendetta
+ */
+public class BarangAntik {
+   int umur;
+    
+    public BarangAntik (int umur){
+        this.umur = umur;
+    }
+    
+    public void tampilUmur (){
+        System.out.println("Umur barang antik ini adalah : "+umur+" tahun");
+    }
+}
---
+++ PBO1-10118031-Latihan56-UmurBarangAntik/src/pbo1/pkg10118031/latihan56/umurbarangantik/PBO110118031Latihan56UmurBarangAntik.java
@@ -0,0 +1,22 @@
+package pbo1.pkg10118031.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author Vendetta 
+ * NAMA : RIZKI ARIA PUTRA 
+ * KELAS : PBO1 
+ * NIM : 10118031
+ * Deskripsi Program : Program ini untuk membuat tampilan umur barang antik.
+ */
+public class PBO110118031Latihan56UmurBarangAntik {
+
+    public static void main(String[] args) {
+
+        Radio radio = new Radio(234);
+        radio.setName("Radio");
+        System.out.println("Nama barang Antik : " + radio.getName());
+        radio.tampilUmur();
+
+    }
+
+}
